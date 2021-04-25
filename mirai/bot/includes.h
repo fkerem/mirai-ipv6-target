b682adae929914dd59419e13f19da5526233f30e
Kerem Ors <karry@Kerems-MacBook-Pro.local>
2021-04-25 06:21:08 +0300

    initial ipv6 settings updated

--- mirai/bot/includes.h
+++ mirai/bot/includes.h
@@ -13,6 +13,11 @@
 typedef char BOOL;
 
 typedef uint32_t ipv4_t;
+
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
+typedef uint128_t ipv6_t;
+
 typedef uint16_t port_t;
 
 #define INET_ADDR(o1,o2,o3,o4) (htonl((o1 << 24) | (o2 << 16) | (o3 << 8) | (o4 << 0)))
